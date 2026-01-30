19dd2a5498e29054afbdca909bf47946492e0e5a
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2026-01-30 13:36:51 -0800

    Arreglos, Instanciar escenas aleatoria

--- scenes/main/asteroide_spawder.gd
+++ scenes/main/asteroide_spawder.gd
@@ -3,11 +3,11 @@ extends Marker2D
 @export var asteroids: Array[PackedScene]
 
 func add_asteroid():
-	
-	#var big_asteroid_instance = big_asteroid_scene.instantiate()
-	#var random_y = randf_range(87, 565)
-	#add_child(big_asteroid_instance)
-	#big_asteroid_instance.global_position.y = random_y
+	var random_asteroid_scene = asteroids.pick_random()
+	var random_asteroid_instance = random_asteroid_scene.instantiate()
+	var random_y = randf_range(87, 565)
+	add_child(random_asteroid_instance)
+	random_asteroid_instance.global_position.y = random_y
 	pass
 
 func _on_timer_timeout() -> void:
